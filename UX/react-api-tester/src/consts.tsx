05ec38b208d2fa9ae31f11cdb7a9f4fb615f8c38
danielray1226 <danielray1226@gmail.com>
2024-10-20 16:58:24 -0500

    mem. and fill up params

--- UX/react-api-tester/src/consts.tsx
+++ UX/react-api-tester/src/consts.tsx
@@ -25,6 +25,14 @@ export function isDev() {
   return import.meta.env.MODE == "development";
 }
 
+/*
+  we can save openApi json object here, to avoid passing it through the props
+*/
+let openApiObject = null;
+export function getOpenApi() {
+  return openApiObject;
+}
+
 export function loadOpenApi(
   onData?: (openApi: Object) => void,
   onError?: (ex) => void
@@ -41,6 +49,7 @@ export function loadOpenApi(
   })
     .then((response) => response.json())
     .then((data) => {
+      openApiObject = data;
       if (onData) onData(data);
     })
     .catch((e) => {
@@ -103,4 +112,165 @@ export function callApi(
       console.log(e);
       if (onError) onError(e);
     });
+}
+
+export function getJsonLeaf(obj: Object, path: string[]) {
+  let currentObject = obj;
+  for (let index = 0; index < path.length; index++) {
+    const nameOfIndex = path[index];
+    let newObj = currentObject[nameOfIndex];
+    if (!newObj) {
+      return null;
+    }
+    currentObject = newObj;
+  }
+  return currentObject;
+}
+
+export function getOpenApiSchemaByRef(ref: string) {
+  const reference = ref.substring(2); // we assume ref is like "#/components/schemas/PagedOptions"
+  const splitted = reference.split("/");
+  //console.log("Reference: ", reference);
+  //console.log("Stringified: ", JSON.stringify(splitted));
+  return getJsonLeaf(getOpenApi(), splitted); // e.g. json object representing: components->schemas->Artifact
+}
+
+export function getOpenApiSchemaPropertiesByRef(ref: string) {
+  const refDef = getOpenApiSchemaByRef(ref); // e.g. json object representing: components->schemas->Artifact
+  if (refDef) return refDef["properties"];
+  return null;
+}
+
+/*
+for arguments ("/projects", "get", "200") will return "#/components/schemas/PagedOptions"
+*/
+export function getOpenApiResponseSchemaRef(
+  path: string,
+  httpMethod: string,
+  statusCode: number
+) {
+  //console.log(getJsonLeaf(getOpenApi(), ["paths", path, httpMethod]));
+  const content = getJsonLeaf(getOpenApi(), [
+    "paths",
+    path,
+    httpMethod,
+    "responses",
+    "" + statusCode,
+    "content",
+  ]); // "paths"->"/projects"->"get"->"responses"->"200"->"content"
+
+  //console.log("Content: ", content);
+
+  if (!content) return null;
+  let schema;
+  for (const [mime, value] of Object.entries(content)) {
+    // we assume only one response mime type, e.g. "application/json" : { "schema" : {"$ref":...}}
+    console.log("extracting first response mime type:", `${mime}: ${value}`);
+    schema = value["schema"];
+    if (schema && schema["$ref"]) return schema["$ref"];
+  }
+  return null;
+}
+
+function isPropertyDefTypeOf(propertyDef: Object, expectedType: string) {
+  const type = propertyDef["type"];
+  if (Array.isArray(type)) {
+    for (const element of type) {
+      if (element == expectedType) return true;
+    }
+  } else {
+    return expectedType == type;
+  }
+}
+
+export function getResultHeadersAndRows(
+  property: string,
+  propertyDef: Object,
+  propertyData: Object
+) {
+  let headers = [];
+  let rows = [];
+
+  /* 
+  lets see, if propertyDef refers to a yet another schema object.
+  we dont really know what are the rules here, assume, that we only need to cover use case
+    {
+      "description": "The items for this page",
+      "items": {
+          "$ref": "#/components/schemas/Option"
+      },
+      "type": "array"
+    }
+  */
+  if (
+    // check, if its an array
+    isPropertyDefTypeOf(propertyDef, "array") &&
+    Array.isArray(propertyData)
+  ) {
+    //
+    console.log("both property type, and the data are arrays");
+    if (propertyDef["items"] && propertyDef["items"]["$ref"]) {
+      const rowRef = propertyDef["items"]["$ref"];
+      const actualProperties = getOpenApiSchemaPropertiesByRef(rowRef);
+      console.log("actualProperties: ", JSON.stringify(actualProperties));
+      headers = Object.keys(actualProperties); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+      rows = propertyData;
+
+      return [headers, rows];
+    }
+  }
+
+  if (
+    isPropertyDefTypeOf(propertyDef, "object") &&
+    typeof propertyData === "object" &&
+    propertyData !== null
+  ) {
+    // it looks like the data is an object, and the type is an object
+    /*
+    case for:
+                      "attributes": {
+                        "additionalProperties": {
+                            
+                        },
+                        "description": "Map of field IDs to their corresponding values.",
+                        "example": {
+                            "formatted_id": "US123",
+                            "id": "1234567",
+                            "revision": "a3f67e2b5f1b5fda5dee3225b644525f",
+                            "summary": "Lorem ipsum"
+                        },
+                        "type": "object"
+                    },
+    */
+    headers = Object.keys(propertyData);
+    rows = [propertyData];
+    return [headers, rows];
+  }
+
+  if (propertyData) {
+    /*
+    case for:
+                        "next_cursor": {
+                        "description": "The cursor for the next page, or not provided if the last element is included in the payload",
+                        "type": [
+                            "string",
+                            "null"
+                        ]
+                    }
+    */
+    headers = [property];
+    let valObj = {};
+    valObj[property] = propertyData;
+    rows = [valObj];
+  }
+
+  console.log(
+    "Extracting headers/rows for property: ",
+    property,
+    ", propertyDef" + JSON.stringify(propertyDef),
+    ", and propertyData:",
+    JSON.stringify(propertyData)
+  );
+
+  return [headers, rows];
 }
